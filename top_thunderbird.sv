5312868421dbda0ad9d4680b58ec814a868c5e44
piper-swain <piper.swain@okstate.edu>
2026-04-22 16:30:14 -0500

    Syntax repairs

--- top_thunderbird.sv
+++ top_thunderbird.sv
@@ -1,12 +1,12 @@
 module top (
     input logic sysclk_125mhz,
     input logic [7:0] sw,
-    input logic [3:0] btn
-    output logic [7:0] led
-    output logic [3:0] sseg_an
+    input logic [3:0] btn,
+    output logic [7:0] led,
+    output logic [3:0] sseg_an,
     output logic [7:0] ssegout
 );
-    logic slow_clk
+    logic slow_clk;
     logic [2:0] mode;
 
     clock_divider #(
