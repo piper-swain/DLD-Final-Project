216f8d72ce674445407dedb5e0e4a98d0951c1f5
aidanguarnera-sys <aidanguarnera@gmail.com>
2026-04-22 16:02:35 -0500

    Refactor top module by removing unnecessary ports
    Removed unused inputs and outputs from the top module.

--- top_thunderbird.sv
+++ top_thunderbird.sv
@@ -1,10 +1,7 @@
 module top (
     input logic sysclk_125mhz,
-    input logic [0:3] btn,
-    output logic [7:0] ssegout,
-    output logic [3:0] sseg_an,
-    input logic haz_sw,
-    output logic [0:5] LED
+    input logic [7:0] sw,
+    input logic [3:0]
 );
 
 
